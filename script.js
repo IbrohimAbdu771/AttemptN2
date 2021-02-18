3f3d9f07ce5d93cf8abf7fb605fe27d37543fcc3
Ibrohim <example@gmail.com>
2021-02-18 22:56:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var number = 5;
+console.log(number);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log("string*9");
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+
+console.log(persone["name"]);
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log(arr[0]);
+
+// alert("Hello world!");
+
+// confirm("Are you here?");
+
+// let answer = prompt("Are you 18?", "Yes");
+
+console.log(typeof arr);
+
+console.log("arr" + " - object");
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isChecked = true,
+  isClose = false;
+
+console.log(isChecked || isClose);
